70450ebb122756fc2104d82b2aafb8c7dcf8db10
Thiago Leite <thiagofmleite@gmail.com>
2024-05-25 13:52:52 -0300

    Adding database setup

---
+++ src/database.ts
@@ -0,0 +1,19 @@
+import { env } from './env'
+import { knex as setupKnex, Knex } from 'knex'
+
+export const config: Knex.Config = {
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite3'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+export const knex = setupKnex(config)
